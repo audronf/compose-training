a9123927aa0f9a779136d90b75fc7dd13d47e60d
ffasitella <federico.d.fasitella@accenture.com>
2022-07-22 12:12:01 -0300

    Added Lottie for like animation

--- app/src/main/java/com/audronf/compose/ui/screens/Details.kt
+++ app/src/main/java/com/audronf/compose/ui/screens/Details.kt
@@ -1,6 +1,8 @@
 package com.audronf.compose.ui.screens
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.rememberScrollState
@@ -14,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +24,15 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionResult
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieAnimatable
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.audronf.compose.model.Movie
 import com.audronf.compose.R
 import com.audronf.compose.ui.components.MovieDetailHeader
@@ -44,60 +55,77 @@ fun Details(
 fun DetailsScreen(navHostController: NavHostController, movie: Movie) {
     val isFavorite = remember { mutableStateOf(false) }
     with(movie) {
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            Row {
-                MovieDetailHeader(navHostController, image, age, duration)
-            }
-            Column(
-                modifier = Modifier
-                    .padding(24.dp)
-                    .fillMaxWidth()
-            ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                 Row {
-                    Text(text = genre.uppercase(), color = Color.Gray, fontSize = 12.sp)
+                    MovieDetailHeader(navHostController, image, age, duration)
                 }
-                Row {
-                    Text(text = name, style = Typography.h5, fontWeight = FontWeight.Bold)
-                }
-                MovieRate(this@with)
-                Spacer(modifier = Modifier.height(16.dp))
-                Row {
-                    Text(text = synopsis, color = Color.Gray, fontSize = 16.sp)
-                }
-                Spacer(modifier = Modifier.height(32.dp))
-                Row() {
-                    val context = LocalContext.current
-                    Button(
-                        onClick = {
-                            if (!isFavorite.value) {
-                                Toast.makeText(context, "Agregada a favoritos", Toast.LENGTH_SHORT)
-                                    .show()
-                            } else {
-                                Toast.makeText(
-                                    context,
-                                    "Eliminada de favoritos",
-                                    Toast.LENGTH_SHORT
-                                )
-                                    .show()
-                            }
-                            isFavorite.value = !isFavorite.value
-                        },
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color(0xFF031939),
-                            contentColor = Color.White
-                        ),
-                        modifier = Modifier.fillMaxWidth()
-                    ) {
-                        Text(
-                            text = if (!isFavorite.value)
-                                stringResource(id = R.string.add_to_favorites).uppercase()
-                            else
-                                stringResource(id = R.string.remove_from_favorites).uppercase(),
-                            fontWeight = FontWeight.Light,
-                            modifier = Modifier.padding(0.dp, 4.dp)
-                        )
+                Column(
+                    modifier = Modifier
+                        .padding(24.dp)
+                        .fillMaxWidth()
+                ) {
+                    Row {
+                        Text(text = genre.uppercase(), color = Color.Gray, fontSize = 12.sp)
+                    }
+                    Row {
+                        Text(text = name, style = Typography.h5, fontWeight = FontWeight.Bold)
+                    }
+                    MovieRate(this@with)
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row {
+                        Text(text = synopsis, color = Color.Gray, fontSize = 16.sp)
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Row {
+                        val context = LocalContext.current
+                        Button(
+                            onClick = {
+                                isFavorite.value = !isFavorite.value
+                            },
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFF031939),
+                                contentColor = Color.White
+                            ),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = if (!isFavorite.value)
+                                    stringResource(id = R.string.add_to_favorites).uppercase()
+                                else
+                                    stringResource(id = R.string.remove_from_favorites).uppercase(),
+                                fontWeight = FontWeight.Light,
+                                modifier = Modifier.padding(0.dp, 4.dp)
+                            )
+                        }
                     }
                 }
+            }
+            Column(modifier = Modifier.align(Alignment.BottomCenter)) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Heart(isFavorite.value)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Heart(isFavorite: Boolean?) {
+    isFavorite?.let {
+        if (it) {
+            val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.hearts))
+            val logoAnimationState =
+                animateLottieCompositionAsState(composition = composition)
+            if (!logoAnimationState.isAtEnd) {
+                LottieAnimation(
+                    composition = composition,
+                    progress = { logoAnimationState.progress },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
     }
